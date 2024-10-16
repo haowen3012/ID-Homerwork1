8b43e6bcb208e726a1e58e451872c9b70254c119
haowenZheng <haowenz01@gmail.com>
2024-10-16 16:55:24 +0200

    refactor extraction

--- homework1/RAG/estraction.py
+++ homework1/RAG/estraction.py
@@ -47,19 +47,18 @@ def extract_tables_from_html_file(html_file, arxiv_id):
         footnotes = table.xpath(f".//cite")
         footnotes_array = []
         if footnotes:
-            for index in  range(len(footnotes)):
+            for index in range(len(footnotes)):
                 footnote_url = footnotes[index].xpath('./a/@href')[0]
                 footnote_id = urlparse(footnote_url).fragment
                 bibItem = tree.xpath(f"//li[@id='{footnote_id}']")[0]
-                footnotes_array.append(etree.tostring(bibItem, method='text', encoding='unicode').strip().replace('&nbsp;', ' '))
-        
+                footnotes_array.append(etree.tostring(bibItem, method='text', encoding='unicode').strip().replace('\u00A0', ' '))
+
+
         # Cerca i paragrafi che contengono un href con il frammento che fa riferimento alla tabella corrente
         references = tree.xpath(f"//p[a[contains(@href, '#{table_id}')]]")
 
         # Estrai il testo dei paragrafi trovati
-        references_text = [
-        etree.tostring(ref, method='text', encoding='unicode').strip() for ref in references
-        ]
+        references_text = [etree.tostring(ref, method='text', encoding='unicode').strip().replace('\u00A0', ' ') for ref in references]
         
         # Store the extracted information in the dictionary
         data[table_id] = {
@@ -72,8 +71,8 @@ def extract_tables_from_html_file(html_file, arxiv_id):
     current_dir = os.path.dirname(__file__)
     sources_rag_path = os.path.join(current_dir, 'extraction')
     # Define output directory
-    #output_dir = r'C:\Users\h.zheng\Documents\Ingegneria dei Dati\ID-Homerworks\homework1\RAG\extraction'
-    output_dir = r'ID-Homerworks\homework1\RAG\extraction'
+    output_dir = r'C:\Users\h.zheng\Documents\Ingegneria dei Dati\ID-Homerworks\homework1\RAG\extraction'
+    #output_dir = r'ID-Homerworks\homework1\RAG\extraction'
     os.makedirs(output_dir, exist_ok=True)
 
     # Save the JSON data to a file named after the arxiv_id
@@ -97,8 +96,8 @@ def process_html_files_in_directory(directory):
 
 def main():
     # Esempio di utilizzo
-    #process_html_files_in_directory(r'C:\Users\h.zheng\Documents\Ingegneria dei Dati\ID-Homerworks\homework1\RAG\sources_RAG')
-    process_html_files_in_directory(r'ID-Homerworks\homework1\RAG\sources_RAG')
+    process_html_files_in_directory(r'C:\Users\h.zheng\Documents\Ingegneria dei Dati\ID-Homerworks\homework1\RAG\sources_RAG')
+    #process_html_files_in_directory(r'ID-Homerworks\homework1\RAG\sources_RAG')
 
 
 if __name__ == "__main__":
